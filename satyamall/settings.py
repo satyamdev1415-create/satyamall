887de73006c46bdd61af96fe309d79dd98055feb
satyamdev1415 <satyamdev1415@gmail.com>
2026-05-11 17:32:46 +0530

    fixed admin

--- satyamall/settings.py
+++ satyamall/settings.py
@@ -75,22 +75,12 @@ WSGI_APPLICATION = 'satyamall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
-import os
-
-if 'RENDER' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            os.environ.get('DATABASE_URL')
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
